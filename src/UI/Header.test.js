5722e8a6d41bc4b24a137555e695577ca18d4a72
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-12-23 14:33:30 +0530

    Testing User Actions

--- src/UI/Header.test.js
+++ src/UI/Header.test.js
@@ -2,35 +2,47 @@ import Header from "./Header";
 import { render, screen } from "@testing-library/react";
 import { Provider } from 'react-redux'; 
 import store from "../../Store/MainStore";
+import userEvent from "@testing-library/user-event";
 
-describe('Header', ()=>{
-    test('renders Welcome text',()=>{
-
-        //Arrange
-        render( <Provider store={store}> {/* Wrap your component with Provider */}
+describe("Header", () => {
+  test("renders Welcome text", () => {
+    render(
+      <Provider store={store}>
+        {" "}
         <Header />
-      </Provider>);
-        //Act
-        //.... nothing
-
-        //Assert
-        const welcomeText = screen.getByText('Welcome To Expense Tracker', {exact: false});
-        expect(welcomeText).toBeInTheDocument();
-
-        const h1text = screen.getByText('test', {exact: false});
-        expect(h1text).toBeInTheDocument();
+      </Provider>
+    );
+    
+    const welcomeText = screen.getByText("Welcome To Expense Tracker", {
+      exact: false,
     });
-    test('h1 text',()=>{
-        //Arrange
-        render( <Provider store={store}> {/* Wrap your component with Provider */}
+    expect(welcomeText).toBeInTheDocument();
+
+    const h1text = screen.getByText("test", { exact: false });
+    expect(h1text).toBeInTheDocument();
+  });
+  test("h1 text", () => {
+    render(
+      <Provider store={store}>
+        {" "}
         <Header />
-      </Provider>);
-        //Act
-        //.... nothing
+      </Provider>
+    );
+    const h1text = screen.getByText("test", { exact: false });
+    expect(h1text).toBeInTheDocument();
+  });
 
-        //Assert
-        const h1text = screen.getByText('test', {exact: false});
-        expect(h1text).toBeInTheDocument();
-
-    });
-})
+  test("premium button", () => {
+    render(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+    const button = screen.queryByRole('button', { name: /Activate Premium/i });
+    if (button) {
+    userEvent.click(button);
+    const outputelement = screen.getByText('Premium Activated', {exact:false});
+    expect(outputelement).toBeInTheDocument();
+    }
+  });
+});
